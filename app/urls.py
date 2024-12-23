aea466fc2a8d2168072a91e34fe7c7eae4f39cfe
Basudev <samantabasu.2001@gmail.com>
2024-12-23 12:43:07 +0530

    Cart section running

--- app/urls.py
+++ app/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
   path("profile/", views.ProfileView.as_view(), name="profile"),
   path("address/", views.address, name="address"),
   path("updateAddress/<int:pk>", views.updateAddress.as_view(), name="updateAddress"),
+  
+  path("add-to-cart/", views.add_to_cart, name="add-to-cart"),
+  path('cart/', views.show_cart, name='showCart'),
 
 # login authentication
 path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
